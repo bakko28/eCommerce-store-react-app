32716d79c50ddd144082fab6e3c0df77aaa8c153
Dmitry Eshman <89666814+bakko28@users.noreply.github.com>
2024-07-09 15:55:38 +0300

    done deleteFunc

--- ecom-app/src/components/header.js
+++ ecom-app/src/components/header.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaCartShopping } from "react-icons/fa6";
+import Order from './order';
 
-const Header = () => {
+const showOrders = (props) => {
+    let summa = 0
+    props.orders.forEach(el => summa += Number.parseFloat(el.price))
+    return (
+        <div>
+        {props.orders.map(el => (
+            <Order onDelete={props.onDelete} key={el.id} item = {el} />
+        ))}
+        <p className='summa'>Сумма: {new Intl.NumberFormat().format(summa)} $</p>
+        </div> 
+    )
+}
+
+const showNothing = () => {
+    return (
+        <div className='empty'>
+            <h2>Ваша корзина пустая.</h2>
+        </div>
+    )
+}
+
+export default function Header(props) {
+    let [cartOpen, setCarOtpen] = useState(false)
+
     return (
         <header>
             <div>
@@ -10,12 +35,15 @@ const Header = () => {
                     <li>Contacts</li>
                     <li>Reviews</li>
                 </ul>
-            </div>
-            <div className='presentation'>
+                <FaCartShopping onClick={() => setCarOtpen(cartOpen = !cartOpen)} className={`shop-cart-btn ${cartOpen && 'active'}`} />
 
+                {cartOpen && (
+                    <div className='shop-cart'>
+                        {props.orders.length > 0 ? showOrders(props) : showNothing()}
+                    </div>
+                )}
             </div>
+            <div className='presentation'></div>
         </header>
     );
 }
-
-export default Header;
--- ecom-app/src/components/order.js
+++ ecom-app/src/components/order.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
+import { FaTrashCan } from "react-icons/fa6";
 
-export class Item extends Component {
+export class Order extends Component {
   render() {
     return (
       <div className='item'>
         <img src={"./img/" + this.props.item.img} alt="Картинка игры" />
         <h2>{this.props.item.title}</h2>
-        <p>{this.props.item.desc}</p>
-        <b>{this.props.item.price} $</b>
-        <div className='add-to-cart' onClick={() => this.props.onAdd(this.props.item)} >+</div>
+        <p>{this.props.item.price} $</p>
+        <FaTrashCan className='delete-icon' onClick={() => this.props.onDelete(this.props.item.id)}/>
       </div>
     )
   }
 }
 
-export default Item
+export default Order
