3070400ad627527556d328ee9f3c0c6946f62ef7
Dmitry Eshman <89666814+bakko28@users.noreply.github.com>
2024-07-09 03:37:02 +0300

    done verst and minimal logic

---
+++ ecom-app/src/App.js
@@ -0,0 +1,75 @@
+import React from "react";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Items from "./components/items";
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Atomic Heart',
+          img: 'atomic-heart.png',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing',
+          category: 'games',
+          price: '25.70'
+        },
+        {
+          id: 2,
+          title: 'Forza Horizon 4',
+          img: 'forza4.png',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing',
+          category: 'games',
+          price: '11.49'
+        },
+        {
+          id: 3,
+          title: 'Battlefield™ 2042',
+          img: 'battlefield.png',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing',
+          category: 'games',
+          price: '51.23'
+        },
+        {
+          id: 4,
+          title: 'Grand Theft Auto V',
+          img: 'gtav.png',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing',
+          category: 'games',
+          price: '16.99'
+        },
+        {
+          id: 5,
+          title: 'Counter-Strike 2',
+          img: 'cs2.png',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing',
+          category: 'games',
+          price: '15.88'
+        },
+        {
+          id: 6,
+          title: 'LEGO Marvel Super Heroes 2',
+          img: 'lego2.png',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing',
+          category: 'games',
+          price: '27.81'
+        },
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items}/>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ ecom-app/src/components/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <span className='logo'>Games staff</span>
+                <ul className='nav'>
+                    <li>About Us</li>
+                    <li>Contacts</li>
+                    <li>Reviews</li>
+                </ul>
+            </div>
+            <div className='presentation'>
+
+            </div>
+        </header>
+    );
+}
+
+export default Header;
